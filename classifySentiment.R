5b2c85852b1180a828e8089e3a40a677aca804d1
Grollus <athoul01@gmail.com>
2015-10-10 08:31:06 -0400

    -results and obtaining prob values

--- classifySentiment.R
+++ classifySentiment.R
@@ -5,23 +5,6 @@ happy_log_probs <- full_word_list[, 1:2]
 sad_log_probs <- full_word_list[, c(1, 3)]
 
 
-classifySentiment <- function(tweet, happy_log_probs = happy_log_probs, sad_log_probs = sad_log_probs){
-  
-  words <- unlist(str_split(tweet, " "))
-  
-  happy_probs <- unlist(lapply(words, function(x) if(x %in% happy_log_probs$word){happy_log_probs$happy[happy_log_probs$word == x]}))
-  sad_probs <- unlist(lapply(words, function(x) if(x %in% sad_log_probs$word){sad_log_probs$sad[sad_log_probs$word == x]}))
-  
-  tweet_happy_log_prob <- sum(happy_probs)
-  tweet_sad_log_prob <- sum(sad_probs)
-  prob_happy <- 1/(exp(tweet_sad_log_prob - tweet_happy_log_prob) + 1)
-  prob_sad <- 1 - prob_happy
-
-  return(c(prob_happy, prob_sad))
-}
-# lapply(clt$text[1:50], function(x) classifySentiment(x, happy_log_probs, sad_log_probs))
-
-
 `%fin%` <- function(x, table){
   stopifnot(require(fastmatch))
   fmatch(x, table, nomatch = 0L) > 0L
@@ -41,9 +24,14 @@ classifySentiment_fast <- function(tweet, happy_log_probs = happy_log_probs, sad
   
   return(c(prob_happy, prob_sad))
 }
-# system.time(lapply(clt$text,function(x) classifySentiment_fast(x, happy_log_probs, sad_log_probs)))
 
-clt <- rbind(clean_net[1:500,], clean_pet[1:500,])
+results <- lapply(clean_pet$text,function(x) classifySentiment_fast(x, happy_log_probs, sad_log_probs))
+results_n <- lapply(clean_net$text, function(x) classifySentiment_fast(x, happy_log_probs, sad_log_probs))
 
-clt_words <- unlist(str_split(clt$text, " "))
-happy_probs <- unlist(lapply(clt_words, function(x) happy_log_probs$happy[happy_log_probs$word == x]))
+probValues <- function(tweet_df, predicted_prob_list){
+  tweet_df$prob_happy <- unlist(lapply(predicted_prob_list, function(x) x[[1]][1]))
+  tweet_df$prob_sad <- unlist(lapply(predicted_prob_list, function(x) x[[1]][2]))
+  return(tweet_df)
+}
+
+net_temp <- probValues(net_temp, results_n)
